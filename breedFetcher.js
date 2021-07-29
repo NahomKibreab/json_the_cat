e1d267ba1a84b3a97131c39a336f3b70851fbb71
Nahom Mehanzel <nknahom@gmail.com>
2021-07-29 21:29:12 +0000

    error checker added

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,6 +2,14 @@ const request = require("request");
 const baseURL = "https://api.thecatapi.com/v1/breeds/search?q=";
 const searchCat = process.argv.slice(2);
 request(`${baseURL}${searchCat}`, (error, response, body) => {
-  const data = JSON.parse(body);
-  console.log(data[0].description);
+  if (error) {
+    console.log("Error", error);
+  } else {
+    const data = JSON.parse(body);
+    if (data.length > 0) {
+      console.log(data[0].description);
+    } else {
+      console.log(`${searchCat} is not found!`);
+    }
+  }
 });
